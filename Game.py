4c8e5a4c5eb2efce5ac8137fbdd1d5571082c2b5
tntse <samoht.est@gmail.com>
2016-06-24 00:32:57 -0700

    fixing more syntax errors, and fixing bug where board would have duplicate rows

--- Game.py
+++ Game.py
@@ -1,4 +1,6 @@
 
+from __future__ import print_function
+
 class Game(object):
 
 	def __init__(self):
@@ -9,20 +11,26 @@ class Game(object):
 		self.lastMove = None
 
 	def newGame(self):
-		self.board = [[None] * 3] * 3
+		self.board = [[None, None, None], 
+					  [None, None, None], 
+					  [None, None, None]]
 		self.winner = None
 		self.lastMove = None
+		return
 	
 	def play(self):
-		keepPlaying = true
+		keepPlaying = True
 
 		while(keepPlaying):
 			self.startGame()
-			print "Congrats! The " + self.winner + "'s win the game!\n"
-			print "Play Again?\n "
-			response = input("Play Again? [Y/N]: ")
-			if(response == "N" or response == "n"):
-				keepPlaying = false
+			print ("Congrats! The ", end="")
+			print(self.winner, end="")
+			print("'s win the game!\n")
+			self.displayBoard()
+
+			response = "" + input("Play Again? [Y/N]: ")
+			if(response == 'N' or response == 'n'):
+				keepPlaying = False
 			else:
 				self.player1 = "X" if self.player1 == "O" else "O"
 				self.player2 = "X" if self.player2 == "O" else "O"
@@ -30,28 +38,33 @@ class Game(object):
 
 
 	def startGame(self):
-		print "Welcome to Text based Tic-Tac-Toe!\n"
-		print "Match 3 in Row to win!\n"
+		print ("Welcome to Text based Tic-Tac-Toe!")
+		print ("Match 3 in Row to win!")
 		
-		newGame()
+		self.newGame()
 		turn = "X"
-		print "Player ", 1 if (self.player1 == "X") else 2, " Starts.\n"
+		print ("Player ", end="")
+		print (1 if (self.player1 == "X") else 2, end="")
+		print (" Starts. ")
 
 		while(self.winner == None):
-			success = false
+			success = False
 			self.displayBoard()
 
 			while(not success):
-				print "Enter move coordinates: \n"
+				print("Player ", end="")
+				print(1 if (self.player1 == turn) else 2, end="")
+				print(" Turn.")
+				print ("Enter move coordinates: ")
 				x = int(input("Enter row: "))
 				y = int(input("Enter column: "))
 				success = self.isLegalMove(x, y)
 
 				if(success):
 					self.move(turn, x, y)
-					turn = self.player2 if turn == self.player1 else player1
+					turn = self.player2 if turn == self.player1 else self.player1
 				else:
-					print "Invalid move, please try again: \n"
+					print ("Invalid move, please try again: ")
 		return
 
 
@@ -59,48 +72,54 @@ class Game(object):
 		self.lastMove = [x, y]
 		self.board[x][y] = marker
 
-		if( self.isWinner()):
+		if( self.isWinner(marker)):
 			self.winner = marker
 		return
 
 
-	def isWinner(self):
+	def isWinner(self, marker):
 
 		x = self.lastMove[0]
 		y = self.lastMove[1]
 
 		# check row
-		if(self.board[x][0] == self.board[x][1] == self.board[x][2]):
-			return true
+		if(self.board[x][0] == self.board[x][1] == self.board[x][2] == marker):
+			return True
 
 		# check column
-		if(self.board[0][y] == self.board[1][y] == self.board[2][y]):
-			return true
+		if(self.board[0][y] == self.board[1][y] == self.board[2][y] == marker):
+			return True
 
 		# check diagonals
 		if(x == y):
-			if(self.board[0][0] == self.board[1][1] == self.board[2][2]):
-				return true
+			if(self.board[0][0] == self.board[1][1] == self.board[2][2] == marker):
+				return True
 
-			if(self.board[0][2] == self.board[1][1] == self.board[2][0]):
-				return true
+			if(self.board[0][2] == self.board[1][1] == self.board[2][0] == marker):
+				return True
 
-		return false
+		return False
 
 	def isLegalMove(self, x, y):
-		if( (0 <= x <= 1) and (0 <= y <= 2)):
+		if( (0 <= x <= 2) and (0 <= y <= 2)):
 			if(self.board[x][y] == None):
-				return true
-		return false
+				return True
+		return False
 
 	def displayBoard(self):
 		for i in range(3):
 			for j in range(3):
 				if(self.board[i][j] == None):
-					print "-"
+					print ("-", end="")
 				else:
-					print self.board[i][j]
+					print (self.board[i][j], end="")
 
 				if(j < 2):
-					print " | "
-			print "\n"
+					print (" | ", end="")
+			print ("")
+
+
+if __name__ == "__main__":
+	game = Game()
+	game.play()
+
